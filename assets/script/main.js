8d106be97259a0b3da4467b5f958a7f5b287e7e1
ianCampos01 <iancampos09008@gmail.com>
2025-04-25 13:58:24 -0300

    archives of shop_of_Cars

---
+++ assets/script/main.js
@@ -0,0 +1,15 @@
+document.getElementById("botaoCustom1").addEventListener("click", function(e){
+    window.location.href = "https://www.linkedin.com/in/ian-campos-124b4a355/";
+});
+
+document.getElementById("botaoCustom2").addEventListener("click", function(e){
+    window.location.href = "https://www.linkedin.com/in/ian-campos-124b4a355/";
+});
+
+document.getElementById("botaoCustom3").addEventListener("click", function(e){
+    window.location.href = "https://www.linkedin.com/in/ian-campos-124b4a355/";
+});
+
+document.getElementById("botaoCustom4").addEventListener("click", function(e){
+    window.location.href = "https://www.linkedin.com/in/ian-campos-124b4a355/";
+});
